ad3134bbd7203580d0d6f48306a8b7b082ff8883
HoggScripts <143638417+HoggScripts@users.noreply.github.com>
2024-04-01 11:25:56 +0100

    added gitignore files
    added appsettings to gitignore

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3bac5c497ebca66a40ad9a0884270eafec4cb3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeeeed49270672a016499ed8afbe046dfb84fffd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
