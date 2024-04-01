3e4843df5b8b674e521d35aee66ee9ac5e241d5c
HoggScripts <143638417+HoggScripts@users.noreply.github.com>
2024-04-01 12:43:10 +0100

    Finished login/roles/jwt all working
    implementation ready to merge

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc617cd3c7e8e5a07bf845f9d2e5e6a16f67771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33225b7adcac7a73387b1907b450dd88220ff6b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
