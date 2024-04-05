e65530445a896f0d5e18665d8eeef37f39a3e205
HoggScripts <143638417+HoggScripts@users.noreply.github.com>
2024-04-05 15:08:25 +0100

    sign in and sign up components
    added components

--- Frontend/reactstore/src/App.js
+++ Frontend/reactstore/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BookList from './components/BookList';
+import SignUpForm from "./components/SignUpForm";
+import SignInForm from "./components/SignInForm";
 
 function App() {
   return (
       <div className="App">
-        <BookList />
+        <SignInForm />
       </div>
   );
 }
